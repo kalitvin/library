6704fd4dd8fd2c860656b73e42ff8a518d39d6a7
Vladimir A. Kalitvin <kalitvin@gmail.com>
2016-10-26 08:39:10 +0300

    some corrections

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -7,9 +7,25 @@
  */
 
 namespace AppBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
 
-
-class User
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
 {
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
 }
